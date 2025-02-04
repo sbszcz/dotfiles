ce5c0009d012b42cf1742baccc869febf2ba76b2
sbszcz <sebastian.barszczewski@gmail.com>
2025-02-04 09:12:00 +0100

    First steps with chezmoi

---
+++ .chezmoiscripts/run_onchange_01-install-packages.sh
@@ -0,0 +1,22 @@
+#!/bin/zsh
+
+# Check if Homebrew is installed
+if command -v brew &> /dev/null
+then
+    echo "Homebrew is already installed. Upgrading..."
+    brew update && brew upgrade
+else
+    echo "Homebrew is not installed. Installing..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# Install packages
+
+# Check if Ghostty is installed
+if command -v ghostty &> /dev/null
+then
+    echo "Ghostty is already installed."
+else
+    echo "Ghostty is not installed. Installing..."
+    brew install --cask ghostty
+fi
