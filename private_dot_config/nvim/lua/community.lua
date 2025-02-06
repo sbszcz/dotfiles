0d3ce1aad72a9af4c1d43cb50fee007c7451c48e
sbszcz <sebastian.barszczewski@gmail.com>
2025-02-06 09:56:31 +0100

    added neovim config

---
+++ private_dot_config/nvim/lua/community.lua
@@ -0,0 +1,17 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.templ" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.ansible" },
+
+  -- import/override with your plugins folder
+}
