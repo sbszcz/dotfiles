3f34408a7461a4062e85109c1de872f9fde2672d
sebastian <sebastian.barszczewski@gmail.com>
2025-09-11 09:35:37 +0200

    neovim adjustments

--- private_dot_config/nvim/lua/community.lua
+++ private_dot_config/nvim/lua/community.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
@@ -6,7 +6,14 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 ---@type LazySpec
 return {
-  "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+	"AstroNvim/astrocommunity",
+	{ import = "astrocommunity.pack.lua" },
+	{ import = "astrocommunity.pack.rust" },
+	{ import = "astrocommunity.pack.yaml" },
+	{ import = "astrocommunity.pack.go" },
+	{ import = "astrocommunity.pack.templ" },
+	{ import = "astrocommunity.pack.tailwindcss" },
+	{ import = "astrocommunity.pack.ansible" }, -- import/override with your plugins folder
+	{ import = "astrocommunity.colorscheme.catppuccin" },
+	{ import = "astrocommunity.recipes.heirline-mode-text-statusline" },
 }
